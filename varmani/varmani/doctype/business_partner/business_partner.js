6be1351081a0021614447f2df4d1930dbee8f4d5
Hemant Pema <Hemant@APTRONICS.CO.ZA>
2017-10-30 21:43:55 +0200

    Deleted test doctype

--- varmani/varmani/doctype/business_partner/business_partner.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2017, Hemant Pema and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Business Partner', {
-	refresh: function(frm) {
-
-	}
-});
