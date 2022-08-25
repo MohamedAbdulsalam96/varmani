1088b832b7c6d2876caf4135ec61727a3262b773
Hemant Pema <hemant@pema.co.za>
2022-08-25 11:05:35 +0200

    Update setup.py
    Updated to meet v14 install

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
-# -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-import os
 
-version = '1.0.0'
+with open("requirements.txt") as f:
+	install_requires = f.read().strip().split("\n")
+
+# get version from __version__ variable in trader/__init__.py
+from trader import __version__ as version
 
 setup(
     name='varmani',
@@ -13,5 +15,5 @@ setup(
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
-    install_requires=("frappe",),
+    install_requires=install_requires
 )
